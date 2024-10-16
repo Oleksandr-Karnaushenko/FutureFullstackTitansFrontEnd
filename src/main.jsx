4fc74c065553203fe56c44e82a3005a547968557
Oleksandr <45110010+Oleksandr-Karnaushenko@users.noreply.github.com>
2024-10-16 07:27:52 +0200

    Revert "Merge pull request #1 from Oleksandr-Karnaushenko/exxin-branch"
    This reverts commit 6f1ed1d227a0337c90cf64ec8f8adac5f8ce570f, reversing
    changes made to 11b998ad70b51aa3a0c750300a8375510423874d.

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import 'modern-normalize';
-import App from "./components/App/App.jsx";
+import App from './components/App/App.jsx';
 import './index.css';
-import { store, persistor } from "./redux/store.js";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </PersistGate>
-      </Provider>
+    <App />
   </React.StrictMode>
 );
