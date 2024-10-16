df3074667d6b14028b394266665ecb1745cbd611
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-10-16 13:25:34 +0200

    comm

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import App from './components/App/App';
+import { Provider } from "react-redux";
+import store, { persistor } from "./redux/store";
 import 'modern-normalize';
-import App from './components/App/App.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { PersistGate } from "redux-persist/integration/react";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
   </React.StrictMode>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,29 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const dummyReducer = (state = {}) => state;
+
+const persistConfig = {
+  key: "root",
+  storage,  
+};
+
+
+const persistedReducer = persistReducer(persistConfig, dummyReducer);
+
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+
+export default store;
