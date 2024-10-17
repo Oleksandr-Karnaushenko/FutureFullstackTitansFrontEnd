15cad602ee638e7cf0040ac1cadd123addb6b3b8
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-10-17 09:37:02 +0000

    update

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,12 +1,9 @@
-import { useSelector } from "react-redux";
 import css from './WelcomePage.module.css';
 import Main from '../../components/Main/Main';
-import { getToken } from "../../redux/auth/authSelectors";
 
 
 export default function WelcomePage(){
-    const isLogedIn = useSelector(getToken);
     return <div>
-    {!isLogedIn && <Main/>}
+        <Main/>
     </div>
 }
