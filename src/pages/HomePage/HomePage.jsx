c0d1089226bc4ab44aa75b6fa73766ae3f80d570
Yuliuus <yuliiamarkovska@gmail.com>
2024-10-18 14:33:12 +0300

    waterRatioPanel created&styled

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,12 @@
 import DailyNorma from '../../components/DailyNorma/DailyNorma';
+import WaterRatioPanel from '../../components/WaterRatioPanel/WaterRatioPanel';
 import css from './HomePage.module.css';
 
 export default function HomePage() {
   return (
     <div className={css.wrapper}>
       <DailyNorma />
-      <div>
-        <p>WaterRatioPanel component</p>
-      </div>
+      <WaterRatioPanel />
     </div>
   );
 }
