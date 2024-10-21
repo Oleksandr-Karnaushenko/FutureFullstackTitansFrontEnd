94d5b59b31af8ad0afca084a9bca69c14b36db8e
Yuliuus <yuliiamarkovska@gmail.com>
2024-10-21 19:04:43 +0300

    css iss + TodayWater added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,19 @@
 import DailyNorma from '../../components/DailyNorma/DailyNorma';
 import WaterRatioPanel from '../../components/WaterRatioPanel/WaterRatioPanel';
 import AddWaterBtn from '../../components/AddWaterBtn/AddWaterBtn';
+import { TodayWater } from '../../components/TodayWater/TodayWater';
 
 import css from './HomePage.module.css';
 
 export default function HomePage() {
   return (
-    <div className={css.wrapper}>
-      <DailyNorma />
-      <WaterRatioPanel />
-      <AddWaterBtn />
+    <div className={css.background}>
+      <div className={css.container}>
+        <DailyNorma />
+        <WaterRatioPanel />
+        <AddWaterBtn />
+        <TodayWater />
+      </div>
     </div>
   );
 }
