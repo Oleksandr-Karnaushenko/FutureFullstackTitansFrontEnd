c8e316ae63395cc86722f62880e25e96ce00544e
Exxin <exxinblood@gmail.com>
2024-10-21 16:34:35 +0300

    add work header(again)

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -3,7 +3,7 @@ const Icon = ({ name, width= 12, height = 12, color = 'currentColor', className}
   return (
     <svg width={width} height={height} fill={color} className={className}>
       {/* Here we're linking to the sprite's ID using the "use" tag */}
-      <use xlinkHref={`/src/assets/img/icons.svg#icon-${name}`} />
+      <use xlinkHref={`/src/assets/images/icons.svg#icon-${name}`} />
     </svg>
   );
 };
