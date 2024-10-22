b33582445b4d1d237b6fbeac692db445269a81bb
YevheniiaYakymenko <152288008+YevheniiaYakymenko@users.noreply.github.com>
2024-10-22 21:00:35 +0200

    add css, fix svg, percent

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,9 +2,9 @@ import DailyNorma from '../../components/DailyNorma/DailyNorma';
 import WaterRatioPanel from '../../components/WaterRatioPanel/WaterRatioPanel';
 import AddWaterBtn from '../../components/AddWaterBtn/AddWaterBtn';
 import { TodayWater } from '../../components/TodayWater/TodayWater';
-import MonthStatsTable from '../../components/MonthStatsTable/MonthStatsTable';
-
+import MonthStatsTable from '../../components/MonthStatsTable/MonthStatsTable.jsx';
 import css from './HomePage.module.css';
+
 // import { TestBtn } from '../../components/TestBtn/TestBtn.jsx';
 
 export default function HomePage() {
