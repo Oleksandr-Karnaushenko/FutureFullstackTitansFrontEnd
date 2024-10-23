cd19e6e3667891a3262c6b29f6ea98074f984477
vsamopyt <v.samopyt14@gmail.com>
2024-10-23 05:29:56 +0200

    a bit of styling

--- src/components/TodayWaterListItem/TodayWaterListItem.jsx
+++ src/components/TodayWaterListItem/TodayWaterListItem.jsx
@@ -1,22 +1,19 @@
-
-import { useState } from "react";
-import Icon from "../Icon/Icon"
-import { TodayWaterModal } from "../TodayWaterModal/TodayWaterModal";
-import { ButtonBtn } from '../ButtonBtn/ButtonBtn'
-
+import { useState } from 'react';
+import Icon from '../Icon/Icon';
+import { TodayWaterModal } from '../TodayWaterModal/TodayWaterModal';
+import { ButtonBtn } from '../ButtonBtn/ButtonBtn';
 
 import css from './TodayWaterListItem.module.css';
 
 export const TodayWaterListItem = ({ waterItem }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleModal = () => {
+    return setIsModalOpen(!isModalOpen);
+  };
 
-const [isModalOpen, setIsModalOpen] = useState(false)
-const toggleModal = ()=>{
-  return setIsModalOpen(!isModalOpen);
-}
-const closeModal = ()=>{
-  setIsModalOpen(false);
-}
-
+  // const closeModal = ()=>{
+  //   setIsModalOpen(false);
+  // }
 
   const { waterVolume, time } = waterItem;
   // const time = date.split('T')[1];
@@ -26,11 +23,9 @@ const closeModal = ()=>{
   return (
     <div className={css.todayWaterListItemContainer}>
       <div className={css.todayWaterListItemValueContainer}>
-      
         {/* <ReactSVG src={svgIcon} className={css.todayWaterListItemValueIcon} /> */}
 
-        <Icon name ={"glass"} className={css.todayWaterListItemValueIconNew}/>
-
+        <Icon name={'glass'} className={css.todayWaterListItemValueIconNew} />
 
         <span className={css.todayWaterListItemValueLiter}>
           {waterVolumeLiter}
@@ -38,47 +33,57 @@ const closeModal = ()=>{
         <span className={css.todayWaterListItemValueTime}> {time}</span>
       </div>
       <div className={css.todayWaterListItemButtonsContainer}>
-        
-        <ButtonBtn type ={'submit'} clasNameBtn={css.buttonBtnEdit} classNameBtnIcon={css.buttonBtnEditIcon} icon={"notebook"} onClick={()=>{console.log("111111")}}/>
-        <ButtonBtn type ={'submit'} clasNameBtn={css.buttonBtnDelete} classNameBtnIcon={css.buttonBtnDeleteIcon}  icon={"trashbox"}  onClick={toggleModal} />
+        <ButtonBtn
+          type={'submit'}
+          clasNameBtn={css.buttonBtnEdit}
+          classNameBtnIcon={css.buttonBtnEditIcon}
+          icon={'notebook'}
+          onClick={() => {
+            // console.log('111111');
+          }}
+        />
+        <ButtonBtn
+          type={'submit'}
+          clasNameBtn={css.buttonBtnDelete}
+          classNameBtnIcon={css.buttonBtnDeleteIcon}
+          icon={'trashbox'}
+          onClick={toggleModal}
+        />
       </div>
 
-      <TodayWaterModal 
-      title="Delete entry"
-      text ="Are you sure you want to delete the entry?"
-      isOpen={isModalOpen}
-      onClose={toggleModal}
+      <TodayWaterModal
+        title="Delete entry"
+        text="Are you sure you want to delete the entry?"
+        isOpen={isModalOpen}
+        onClose={toggleModal}
       >
-        <ButtonBtn 
+        <ButtonBtn
           // classNameBtnIcon={css.buttonSubmitAddIcon}
           clasNameBtn={css.buttonModalDelete}
           icon={null}
           name={'Delete'}
-          type={"button"}
+          type={'button'}
           // onClick={toggleModal}
-          />
-          
-        <ButtonBtn 
+        />
+
+        <ButtonBtn
           // classNameBtnIcon={css.buttonSubmitAddIcon}
           clasNameBtn={css.buttonModalCancel}
           icon={null}
           name={'Cancel'}
-          type={"button"}
+          type={'button'}
           onClick={toggleModal}
-          />
+        />
 
-<ButtonBtn 
+        <ButtonBtn
           classNameBtnIcon={css.buttonModalIcon}
           clasNameBtn={css.buttonModalClose}
-          icon={"cross"}
+          icon={'cross'}
           // name={'Close'}
-          type={"button"}
+          type={'button'}
           onClick={toggleModal}
-          />
-
+        />
       </TodayWaterModal>
-
-     
     </div>
   );
 };
