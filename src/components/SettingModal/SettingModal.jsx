5b43f28716649ec546e12b94b9ea155b996eef2b
Exxin <exxinblood@gmail.com>
2024-10-23 20:41:57 +0300

    fix modal 2

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -1,13 +1,12 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
-import { useEffect, useState } from 'react';
 import { useState } from 'react';
 import { IoCloseOutline } from 'react-icons/io5';
 import { HiOutlineEyeSlash, HiOutlineEye } from 'react-icons/hi2';
 import { useDispatch, useSelector } from 'react-redux';
-  fetchUserDataAPI,
-} from '../../redux/auth/authOperation.js';
+import { changeUserDataAPI } from '../../redux/auth/authOperation.js';
+import { useInfoValidationSchema } from '../../validation/userValidation.js';
 import { selectCurrentUser } from '../../redux/auth/authSelectors.js';
-import Icon from '../Icon/Icon.jsx';
+import styles from './SettingModal.module.css';
 
 function SettingModal({ isOpen, onClose }) {
   const [showPassword, setShowPassword] = useState(false);
@@ -17,15 +16,11 @@ function SettingModal({ isOpen, onClose }) {
     setShowPassword(!showPassword);
   };
 
-  useEffect(() => {
-    dispatch(fetchUserDataAPI());
-  }, [dispatch]);
-
   const user = useSelector(selectCurrentUser);
   const { name, gender, email } = user;
 
   const initialValues = {
-    gender: gender || '',
+    gender: gender || 'female',
     name: name || '',
     email: email || '',
     oldPassword: '',
@@ -35,201 +30,111 @@ function SettingModal({ isOpen, onClose }) {
 
   const handleSubmit = async (values, { setSubmitting, resetForm }) => {
     try {
-      console.log(values);
-      await dispatch(
-        changeUserDataAPI({
-          name: values.name,
-          gender: values.gender,
-        })
-      ).unwrap();
-      resetForm();
+      const updatedData = {};
+
+      if (values.name !== user.name) updatedData.name = values.name;
+      if (values.gender !== user.gender) updatedData.gender = values.gender;
+      if (values.email !== user.email) updatedData.email = values.email;
+
+      if (values.oldPassword && values.password && values.confirmPassword) {
+        if (values.password === values.confirmPassword) {
+          updatedData.oldPassword = values.oldPassword;
+          updatedData.password = values.password;
+        } else {
+          throw new Error('Passwords do not match');
+        }
+      }
+
+      const userId = user._id;
+      await dispatch(changeUserDataAPI({ userId, userNewData: updatedData })).unwrap();
+      resetForm({ values: { ...initialValues, ...updatedData } });
     } catch (error) {
-      console.error('Backend response:', error.response?.data || error.message);
+      console.error('Error:', error);
     } finally {
       setSubmitting(false);
     }
   };
 
-  // Якщо `isOpen` - false, модалка не рендериться
-  if (!isOpen) {
-    return null;
-  }
+  if (!isOpen) return null; // Якщо модальне вікно закрите не рендеримо його
 
   return (
-    <div className={styles.modal}>
-      <div className={styles.titleWrapper}>
-        <h1 className={styles.title}>Setting</h1>
-        <button className={styles.buttonClose} onClick={onClose}>
-          <Icon
-            name="icon-cross"
-            width={24}
-            height={24}
-            color="#407BFF"
-            className={styles.iconClose}
-          />
-        </button>
-      </div>
-
-      <Formik
-        initialValues={initialValues}
-        onSubmit={handleSubmit}
-      >
-        {({ errors, touched, isSubmitting }) => (
-          <Form className={styles.form}>
-            <div className={styles.form}>
-              <div className={styles.partWrapper}>
-                <label htmlFor="gender" className={styles.labelTitle}>
-                  Your gender identity
-                </label>
-                <div className={styles.genderWrapper}>
-                  <label className={styles.labelField}>
-                    <Field name="gender" type="radio" value="female"></Field>{' '}
-                    Woman
-                  </label>
-                  <label className={styles.labelField}>
-                    <Field name="gender" type="radio" value="male"></Field> Man
-                  </label>
+    <div className={styles.modalOverlay} onClick={onClose}>
+      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.titleWrapper}>
+          <h1 className={styles.title}>Setting</h1>
+          <button className={styles.buttonClose} onClick={onClose}>
+            <IoCloseOutline name="icon-cross" width={24} height={24} color="#407BFF" className={styles.iconClose} />
+          </button>
+        </div>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={useInfoValidationSchema}>
+          {({ errors, touched, isSubmitting }) => (
+            <Form className={styles.form}>
+              <div className={styles.wrapper}>
+                <div className={styles.formWrapper}>
+                  <div className={styles.partWrapper}>
+                    <label htmlFor="gender" className={styles.labelTitle}>Your gender identity</label>
+                    <div className={styles.genderWrapper}>
+                      <label className={styles.labelField}>
+                        <Field name="gender" type="radio" value="female" className={styles.radioInput} />
+                        Woman
+                      </label>
+                      <label className={styles.labelField}>
+                        <Field name="gender" type="radio" value="male" className={styles.radioInput} />
+                        Man
+                      </label>
+                    </div>
+                  </div>
+                  <div className={styles.fieldWrapper}>
+                    <label htmlFor="name" className={styles.labelTitle}>Your name</label>
+                    <Field
+                      name="name"
+                      type="text"
+                      placeholder="Name"
+                      className={`${styles.input} ${errors.name && touched.name ? styles.inputError : ''}`}
+                    />
+                    <ErrorMessage name="name" component="span" className={styles.errorMessage} />
+                  </div>
+                  <div className={styles.fieldWrapper}>
+                    <label htmlFor="email" className={styles.labelTitle}>E-mail</label>
+                    <Field
+                      name="email"
+                      type="text"
+                      placeholder="Email"
+                      className={`${styles.input} ${errors.email && touched.email ? styles.inputError : ''}`}
+                    />
+                    <ErrorMessage name="email" component="span" className={styles.errorMessage} />
+                  </div>
+                </div>
+                <div className={styles.partWrapper}>
+                  <label className={styles.labelTitle}>Password</label>
+                  {['oldPassword', 'password', 'confirmPassword'].map((field, index) => (
+                    <div className={styles.fieldWrapper} key={index}>
+                      <label htmlFor={field} className={styles.labelField}>
+                        {field === 'oldPassword' && 'Outdated password:'}
+                        {field === 'password' && 'New Password:'}
+                        {field === 'confirmPassword' && 'Repeat new password:'}
+                      </label>
+                      <Field
+                        name={field}
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder="Password"
+                        className={`${styles.input} ${errors[field] && touched[field] ? styles.inputError : ''}`}
+                      />
+                      {showPassword ? (
+                        <HiOutlineEyeSlash className={styles.iconEye} onClick={togglePasswordVisibility} />
+                      ) : (
+                        <HiOutlineEye className={styles.iconEye} onClick={togglePasswordVisibility} />
+                      )}
+                      <ErrorMessage name={field} component="span" className={styles.errorMessage} />
+                    </div>
+                  ))}
                 </div>
               </div>
-
-              <div className={styles.fieldWrapper}>
-                <label htmlFor="name" className={styles.labelTitle}>
-                  Your name
-                </label>
-                <Field
-                  name="name"
-                  type="text"
-                  placeholder="Name"
-                  className={styles.input}
-                ></Field>
-                <ErrorMessage
-                  name="name"
-                  component="span"
-                  className={`${styles.errorMessage} ${
-                    errors.name && touched.name ? styles.inputError : ''
-                  }`}
-                />
-              </div>
-
-              <div className={styles.fieldWrapper}>
-                <label htmlFor="email" className={styles.labelTitle}>
-                  E-mail
-                </label>
-                <Field
-                  name="email"
-                  type="text"
-                  placeholder="Email"
-                  className={styles.input}
-                ></Field>
-                <ErrorMessage
-                  name="email"
-                  component="span"
-                  className={`${styles.errorMessage} ${
-                    errors.email && touched.email ? styles.inputError : ''
-                  }`}
-                />
-              </div>
-            </div>
-
-            <div className={styles.partWrapper}>
-              <label className={styles.labelTitle}>Password</label>
-
-              <div className={styles.fieldWrapper}>
-                <label htmlFor="oldPassword" className={styles.labelField}>
-                  Outdated password:
-                </label>
-                <Field
-                  name="oldPassword"
-                  type={showPassword ? 'text' : 'password'}
-                  placeholder="Password"
-                  className={styles.input}
-                ></Field>
-                <Icon
-                  name={showPassword ? 'icon-close-eye' : 'icon-open-eye'}
-                  width={16}
-                  height={16}
-                  color="#407BFF"
-                  className={styles.iconEye}
-                  onClick={togglePasswordVisibility}
-                />
-                <ErrorMessage
-                  name="oldPassword"
-                  component="span"
-                  className={`${styles.errorMessage} ${
-                    errors.oldPassword && touched.oldPassword
-                      ? styles.inputError
-                      : ''
-                  }`}
-                />
-              </div>
-
-              <div className={styles.fieldWrapper}>
-                <label htmlFor="password" className={styles.labelField}>
-                  New Password:
-                </label>
-                <Field
-                  name="password"
-                  type={showPassword ? 'text' : 'password'}
-                  placeholder="Password"
-                  className={styles.input}
-                ></Field>
-                <Icon
-                  name={showPassword ? 'icon-close-eye' : 'icon-open-eye'}
-                  width={16}
-                  height={16}
-                  color="#407BFF"
-                  className={styles.iconEye}
-                  onClick={togglePasswordVisibility}
-                />
-                <ErrorMessage
-                  name="password"
-                  component="span"
-                  className={`${styles.errorMessage} ${
-                    errors.password && touched.password ? styles.inputError : ''
-                  }`}
-                />
-              </div>
-
-              <div className={styles.fieldWrapper}>
-                <label htmlFor="confirmPassword" className={styles.labelField}>
-                  Repeat new password:
-                </label>
-                <Field
-                  name="confirmPassword"
-                  type={showPassword ? 'text' : 'password'}
-                  placeholder="Password"
-                  className={styles.input}
-                ></Field>
-                <Icon
-                  name={showPassword ? 'icon-close-eye' : 'icon-open-eye'}
-                  width={16}
-                  height={16}
-                  color="#407BFF"
-                  className={styles.iconEye}
-                  onClick={togglePasswordVisibility}
-                />
-                <ErrorMessage
-                  name="confirmPassword"
-                  component="span"
-                  className={`${styles.errorMessage} ${
-                    errors.confirmPassword && touched.confirmPassword
-                      ? styles.inputError
-                      : ''
-                  }`}
-                />
-              </div>
-            </div>
-
-            <button
-              type="submit"
-              className={styles.button}
-              disabled={isSubmitting}
-            >
-              Save
-            </button>
-          </Form>
-        )}
-      </Formik>
+              <button type="submit" className={styles.button} disabled={isSubmitting}>Save</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }
