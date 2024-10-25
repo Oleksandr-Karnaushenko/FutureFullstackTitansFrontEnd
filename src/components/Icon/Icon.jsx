e4c1c6357b25db595906b02291fc3f9f9c5cbb50
vsamopyt <v.samopyt14@gmail.com>
2024-10-25 14:39:05 +0200

    icons to piublic

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -3,7 +3,7 @@ const Icon = ({ name, width= 0, height = 0, color = 'currentColor', className}) 
   return (
     <svg width={width} height={height} fill={color} className={className}>
       {/* Here we're linking to the sprite's ID using the "use" tag */}
-      <use xlinkHref={`/src/assets/images/icons.svg#icon-${name}`} />
+      <use xlinkHref={`/assets/images/icons.svg#icon-${name}`} />
     </svg>
   );
 };
