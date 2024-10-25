050098806f305d3211452eaf9f5bd545e82ccff1
Oleksandra <sashakirina@gmail.com>
2024-10-25 11:21:50 +0200

    add upload photo

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -1,6 +1,6 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
+import { toast } from 'react-toastify';
 import styles from './SettingModal.module.css';
 
 import Loader from '../Loader/Loader.jsx';
@@ -15,6 +15,7 @@ import {
   selectAuthIsRefreshing,
   selectCurrentUser,
 } from '../../redux/auth/authSelectors.js';
+import UploadAvatar from '../UploadAvatar/UploadAvatar.jsx';
 
 function SettingModal({ isOpen, onClose }) {
   const [showPassword, setShowPassword] = useState(false);
@@ -90,149 +91,153 @@ function SettingModal({ isOpen, onClose }) {
 
   if (!isOpen) return null;
 
-  return isRefreshing ? (
-    <Loader className={styles.loader} />
-  ) : (
+  return (
     <div className={styles.modalOverlay} onClick={onClose}>
-      <div className={styles.modal} onClick={e => e.stopPropagation()}>
-        <div className={styles.titleWrapper}>
-          <h2 className={styles.title}>Setting</h2>
-          <button className={styles.buttonClose} onClick={onClose}>
-            <IoCloseOutline className={styles.iconClose} />
-          </button>
-        </div>
+      {isRefreshing ? (
+        <Loader className={styles.loader} />
+      ) : (
+        <div className={styles.modal} onClick={e => e.stopPropagation()}>
+          <div className={styles.titleWrapper}>
+            <h2 className={styles.title}>Setting</h2>
+            <button className={styles.buttonClose} onClick={onClose}>
+              <IoCloseOutline className={styles.iconClose} />
+            </button>
+          </div>
 
-        <Formik
-          initialValues={initialValues}
-          onSubmit={handleSubmit}
-          validationSchema={useInfoValidationSchema}
-        >
-          {({ errors, touched, isSubmitting }) => (
-            <Form className={styles.form}>
-              <div className={styles.wrapper}>
-                <div className={styles.formWrapper}>
-                  <div className={styles.partWrapper}>
-                    <label htmlFor="gender" className={styles.labelTitle}>
-                      Your gender identity
-                    </label>
+          <UploadAvatar />
 
-                    <div className={styles.genderWrapper}>
-                      <label className={styles.labelGender}>
-                        <Field
-                          name="gender"
-                          type="radio"
-                          value="female"
-                          className={styles.radioInput}
-                        />
-                        {''}
-                        <span>Woman</span>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            validationSchema={useInfoValidationSchema}
+          >
+            {({ errors, touched, isSubmitting }) => (
+              <Form className={styles.form}>
+                <div className={styles.wrapper}>
+                  <div className={styles.formWrapper}>
+                    <div className={styles.partWrapper}>
+                      <label htmlFor="gender" className={styles.labelTitle}>
+                        Your gender identity
                       </label>
 
-                      <label className={styles.labelGender}>
-                        <Field
-                          name="gender"
-                          type="radio"
-                          value="male"
-                          className={styles.radioInput}
-                        />
-                        {''}
-                        <span>Man</span>
+                      <div className={styles.genderWrapper}>
+                        <label className={styles.labelGender}>
+                          <Field
+                            name="gender"
+                            type="radio"
+                            value="female"
+                            className={styles.radioInput}
+                          />
+                          {''}
+                          <span>Woman</span>
+                        </label>
+
+                        <label className={styles.labelGender}>
+                          <Field
+                            name="gender"
+                            type="radio"
+                            value="male"
+                            className={styles.radioInput}
+                          />
+                          {''}
+                          <span>Man</span>
+                        </label>
+                      </div>
+                    </div>
+
+                    <div className={styles.fieldWrapper}>
+                      <label htmlFor="name" className={styles.labelTitle}>
+                        Your name
                       </label>
+                      <Field
+                        name="name"
+                        type="text"
+                        placeholder="Name"
+                        className={`${styles.input} ${
+                          errors.name && touched.name ? styles.inputError : ''
+                        }`}
+                      />
+                      <ErrorMessage
+                        name="name"
+                        component="span"
+                        className={styles.errorMessage}
+                      />
+                    </div>
+                    <div className={styles.fieldWrapper}>
+                      <label htmlFor="email" className={styles.labelTitle}>
+                        E-mail
+                      </label>
+                      <Field
+                        name="email"
+                        type="text"
+                        placeholder="Email"
+                        className={`${styles.input} ${
+                          errors.email && touched.email ? styles.inputError : ''
+                        }`}
+                      />
+                      <ErrorMessage
+                        name="email"
+                        component="span"
+                        className={styles.errorMessage}
+                      />
                     </div>
                   </div>
 
-                  <div className={styles.fieldWrapper}>
-                    <label htmlFor="name" className={styles.labelTitle}>
-                      Your name
-                    </label>
-                    <Field
-                      name="name"
-                      type="text"
-                      placeholder="Name"
-                      className={`${styles.input} ${
-                        errors.name && touched.name ? styles.inputError : ''
-                      }`}
-                    />
-                    <ErrorMessage
-                      name="name"
-                      component="span"
-                      className={styles.errorMessage}
-                    />
-                  </div>
-                  <div className={styles.fieldWrapper}>
-                    <label htmlFor="email" className={styles.labelTitle}>
-                      E-mail
-                    </label>
-                    <Field
-                      name="email"
-                      type="text"
-                      placeholder="Email"
-                      className={`${styles.input} ${
-                        errors.email && touched.email ? styles.inputError : ''
-                      }`}
-                    />
-                    <ErrorMessage
-                      name="email"
-                      component="span"
-                      className={styles.errorMessage}
-                    />
+                  <div className={styles.partWrapper}>
+                    <label className={styles.labelTitle}>Password</label>
+                    {['oldPassword', 'password', 'confirmPassword'].map(
+                      (field, index) => (
+                        <div className={styles.fieldWrapper} key={index}>
+                          <label htmlFor={field} className={styles.labelField}>
+                            {field === 'oldPassword' && 'Outdated password:'}
+                            {field === 'password' && 'New Password:'}
+                            {field === 'confirmPassword' &&
+                              'Repeat new password:'}
+                          </label>
+                          <Field
+                            name={field}
+                            type={showPassword ? 'text' : 'password'}
+                            placeholder="Password"
+                            className={`${styles.input} ${
+                              errors[field] && touched[field]
+                                ? styles.inputError
+                                : ''
+                            }`}
+                          />
+                          {showPassword ? (
+                            <HiOutlineEyeSlash
+                              className={styles.iconEye}
+                              onClick={togglePasswordVisibility}
+                            />
+                          ) : (
+                            <HiOutlineEye
+                              className={styles.iconEye}
+                              onClick={togglePasswordVisibility}
+                            />
+                          )}
+                          <ErrorMessage
+                            name={field}
+                            component="span"
+                            className={styles.errorMessage}
+                          />
+                        </div>
+                      )
+                    )}
                   </div>
                 </div>
 
-                <div className={styles.partWrapper}>
-                  <label className={styles.labelTitle}>Password</label>
-                  {['oldPassword', 'password', 'confirmPassword'].map(
-                    (field, index) => (
-                      <div className={styles.fieldWrapper} key={index}>
-                        <label htmlFor={field} className={styles.labelField}>
-                          {field === 'oldPassword' && 'Outdated password:'}
-                          {field === 'password' && 'New Password:'}
-                          {field === 'confirmPassword' &&
-                            'Repeat new password:'}
-                        </label>
-                        <Field
-                          name={field}
-                          type={showPassword ? 'text' : 'password'}
-                          placeholder="Password"
-                          className={`${styles.input} ${
-                            errors[field] && touched[field]
-                              ? styles.inputError
-                              : ''
-                          }`}
-                        />
-                        {showPassword ? (
-                          <HiOutlineEyeSlash
-                            className={styles.iconEye}
-                            onClick={togglePasswordVisibility}
-                          />
-                        ) : (
-                          <HiOutlineEye
-                            className={styles.iconEye}
-                            onClick={togglePasswordVisibility}
-                          />
-                        )}
-                        <ErrorMessage
-                          name={field}
-                          component="span"
-                          className={styles.errorMessage}
-                        />
-                      </div>
-                    )
-                  )}
-                </div>
-              </div>
-
-              <button
-                type="submit"
-                className={styles.button}
-                disabled={isSubmitting}
-              >
-                Save
-              </button>
-            </Form>
-          )}
-        </Formik>
-      </div>
+                <button
+                  type="submit"
+                  className={styles.button}
+                  disabled={isSubmitting}
+                >
+                  Save
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      )}
     </div>
   );
 }
